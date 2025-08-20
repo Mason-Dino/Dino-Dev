0598ebc948d6d780d561aa02f9a4a4b492763619
Mason-Dino <mason.n.dinosaur@gmail.com>
2025-08-20 02:48:21 +0000

    working on main home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,12 @@
 export default function Home() {
 	return (
-		<>
-			Joe
-		</>
+		<section className="py-20 px-6">
+			<div className="max-w-full m-auto text-center">
+				<h1 className="text-4xl md:text-6xl font-bold text-white mb-6">
+					Creating Fun Applications
+				</h1>
+				<h3 className="text-xl md:text-3xl font-bold text-white px-20">Creating family fun application to spend<br />time together and grow closer together</h3>
+			</div>
+		</section>
 	);
 }
