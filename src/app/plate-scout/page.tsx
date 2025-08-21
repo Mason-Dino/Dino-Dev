1e52b4c64f95ee8da9aed1ed55aa033b985cfb5a
Mason-Dino <mason.n.dinosaur@gmail.com>
2025-08-21 21:24:19 +0000

    working on homepage of plate scout

--- src/app/plate-scout/page.tsx
+++ src/app/plate-scout/page.tsx
@@ -15,12 +15,19 @@ export default function Home() {
 						</h3>
 					</div>
 					<div className='flex flex-col sm:flex-row gap-4 justify-center'>
-						<button className="rounded-2xl bg-gradient-to-r from-blue-400 to-green-400 hover:from-blue-400 hover:to-blue-400 transition-all transform text-white text-xl font-semibold hover:font-bold py-3 px-10 flex items-center justify-center">
-							Download <ChevronRight />
-						</button>
+                        <a>
+                            <button className="rounded-2xl bg-gradient-to-r from-blue-400 to-green-400 hover:from-blue-400 hover:to-blue-400 transition-all transform text-white text-xl font-semibold hover:font-bold py-3 px-10 flex items-center justify-center">
+                                Download <ChevronRight />
+                            </button>
+                        </a>
 					</div>
 				</div>
 			</section>
+            <section className="flex flex-col text-center justify-content items-center">
+                <h1 className="text-2xl md:text-2xl font-bold text-white mb-6">
+                    What is Plate Scout?
+                </h1>
+            </section>
 		</div>
 	);
 }
