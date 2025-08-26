e1a0076ac1e0dc0cabffa3fab088752bbded8cd7
Mason-Dino <mason.n.dinosaur@gmail.com>
2025-08-26 20:35:07 +0000

    added styling to this page

--- src/app/plate-scout/privacy-policy/page.tsx
+++ src/app/plate-scout/privacy-policy/page.tsx
@@ -18,33 +18,33 @@ export default function Home() {
 			</section>
             <section className='text-white'>
                 <p className='mb-5 italic'>Last Updated: 8/25/25</p>
-                <h3 className='text-3xl font-bold'>Your Privacy Matters</h3>
-                <p>Dinosaur Development is committed to protecting your privacy and so you know where your data is going. This policy will explain what information is stored and how we used that stored data.</p>
-                <h3 className='text-3xl font-bold'>What Information We Collect</h3>
+                <h3 className='text-3xl font-bold mb-3'>Your Privacy Matters</h3>
+                <p className='mb-6'>Dinosaur Development is committed to protecting your privacy and so you know where your data is going. This policy will explain what information is stored and how we used that stored data.</p>
+                <h3 className='text-3xl font-bold mb-3'>What Information We Collect</h3>
                 <p>When you use our app, we are only collecting information you choose to give.</p>
-                <ul>
-                    <li><b>Trip Name</b> - This the name of the trip</li>
-                    <li><b>States Visited</b> - Which states you visited</li>
-                    <li><b>Trip Start Date</b> - The date in which your trip starts</li>
-                    <li><b>Trip Icon</b> - The icon you choose to represent if that is a car, bus, or train</li>
+                <ul className='list-disc mb-6'>
+                    <li className='ml-6'><b>Trip Name</b> - This the name of the trip</li>
+                    <li className='ml-6'><b>States Visited</b> - Which states you visited</li>
+                    <li className='ml-6'><b>Trip Start Date</b> - The date in which your trip starts</li>
+                    <li className='ml-6'><b>Trip Icon</b> - The icon you choose to represent if that is a car, bus, or train</li>
                 </ul>
-                <h3 className='text-3xl font-bold'>What Information do we store</h3>
+                <h3 className='text-3xl font-bold mb-3'>What Information do we store</h3>
                 <p><b>All of your data stays on your device.</b> We do not:</p>
-                <ul>
-                    <li>Send your data to our servers</li>
-                    <li>Store your data in the cloud</li>
-                    <li>Share your data with third parties</li>
-                    <li>Access your data remotely</li>
+                <ul className='list-disc mb-3'>
+                    <li className='ml-6'>Send your data to our servers</li>
+                    <li className='ml-6'>Store your data in the cloud</li>
+                    <li className='ml-6'>Share your data with third parties</li>
+                    <li className='ml-6'>Access your data remotely</li>
                 </ul>
-                <p>The trip information is stored locally on your device using the built-in secure device storage</p>
-                <h3 className='text-3xl font-bold'>Data Deletion</h3>
-                <p>When you delete a trip in the app, <b>all of the associated data is permanently deleted</b> from your device. We cannot recover this data once the delete action is completed</p>
-                <h3 className='text-3xl font-bold'>Account Required</h3>
-                <p>Plate Scout does not require you to create any sort of account or provide personal information</p>
-                <h3 className='text-3xl font-bold'>Updates to this Policy</h3>
-                <p>If we updated this policy you will be notified within the app and the "Last Updated" will be updated with the new updated date.</p>
-                <h3 className='text-3xl font-bold'>Contact Us</h3>
-                <p>If you have any questions about this privacy policy, please submit a ticket at this <a className='underline' href="https://forms.gle/PWzwDehsrpQAYecq8" target='_blank'>page</a></p>
+                <p className='mb-6'>The trip information is stored locally on your device using the built-in secure device storage</p>
+                <h3 className='text-3xl font-bold mb-3'>Data Deletion</h3>
+                <p className='mb-6'>When you delete a trip in the app, <u><b>all of the associated data is permanently deleted</b></u> from your device. We cannot recover this data once the delete action is completed</p>
+                <h3 className='text-3xl font-bold mb-3'>Account Required</h3>
+                <p className='mb-6'>Plate Scout does not require you to create any sort of account or provide personal information</p>
+                <h3 className='text-3xl font-bold mb-3'>Updates to this Policy</h3>
+                <p className='mb-6'>If we updated this policy you will be notified within the app and the "Last Updated" will be updated with the new updated date.</p>
+                <h3 className='text-3xl font-bold mb-3'>Contact Us</h3>
+                <p className='mb-6'>If you have any questions about this privacy policy, please submit a ticket at this <a className='underline' href="https://forms.gle/PWzwDehsrpQAYecq8" target='_blank'>page</a></p>
                 <br /> <br />
                 <p className='mb-5 italic'>This privacy policy applies to v1.0.0 and later versions of Plate Scout</p>
             </section>
