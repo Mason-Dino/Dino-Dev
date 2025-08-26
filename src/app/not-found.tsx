3e6e0b36c7aadb9aaecebe6c81c416e22ab3969b
Mason-Dino <mason.n.dinosaur@gmail.com>
2025-08-26 21:08:17 +0000

    added a 404 page

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -1,7 +1,15 @@
+import { ShieldAlert } from "lucide-react";
+
 export default function NotFount() {
 	return (
-		<div>
-			
-		</div>
+        <section className="flex pt-28 items-center justify-center">
+            <div className="w-fit flex flex-row gap-3">
+                <div><ShieldAlert className="text-white size-20"/></div>
+                <div className="text-white flex flex-col gap-2">
+                    <h2 className="font-bold text-4xl">You got Lost!</h2>
+                    <p className="text-xl">Head back to the <a href="/" className="underline">homepage</a>.</p>
+                </div>
+            </div>
+        </section>
 	);
 }
