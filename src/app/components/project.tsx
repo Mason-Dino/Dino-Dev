743ac2194cf985947e5f58dc7fa81ddcc4e316e4
Mason-Dino <mason.n.dinosaur@gmail.com>
2025-09-04 23:28:25 +0000

    added a new icon and made frameworks optional

--- src/app/components/project.tsx
+++ src/app/components/project.tsx
@@ -1,4 +1,4 @@
-import { Caravan, FolderGit, CodeXml, Download, Globe } from "lucide-react";
+import { Caravan, FolderGit, CodeXml, Download, Globe, BookA } from "lucide-react";
 import { Url } from "next/dist/shared/lib/router/router";
 
 import Link from 'next/link'
@@ -6,7 +6,7 @@ import Link from 'next/link'
 interface ProjectProps {
     Name: string;
     Description: string;
-    Frameworks: string[];
+    Frameworks?: string[];
     Icon: string;
 
     code: boolean;
@@ -23,23 +23,25 @@ export default function Project({Name, Description, Frameworks, Icon, code, down
 
     let IconContent;
 
-    for (let i = 0; i < Frameworks.length; i ++) {
-        console.log(Frameworks[i])
-
-        if (Frameworks[i] === "Swift") {
-            DetailFramework.push(["Swift", "bg-green-500/20 text-green-300 px-3 py-1 rounded-full text-sm w-fit"])
-        }
-
-        if (Frameworks[i] === "SwiftUI") {
-            DetailFramework.push(["SwiftUI", "bg-orange-500/20 text-orange-300 px-3 py-1 rounded-full text-sm w-fit"])
-        }
-
-        if (Frameworks[i] === "JavaScript") {
-            DetailFramework.push(["JavaScript", "bg-yellow-500/20 text-yellow-300 px-3 py-1 rounded-full text-sm w-fit"])
-        }
-
-        if (Frameworks[i] === "Electron.js") {
-            DetailFramework.push(["Electron.js", "bg-blue-500/20 text-blue-300 px-3 py-1 rounded-full text-sm w-fit"])
+    if (Frameworks != null) {
+        for (let i = 0; i < Frameworks.length; i ++) {
+            console.log(Frameworks[i])
+    
+            if (Frameworks[i] === "Swift") {
+                DetailFramework.push(["Swift", "bg-green-500/20 text-green-300 px-3 py-1 rounded-full text-sm w-fit"])
+            }
+    
+            if (Frameworks[i] === "SwiftUI") {
+                DetailFramework.push(["SwiftUI", "bg-orange-500/20 text-orange-300 px-3 py-1 rounded-full text-sm w-fit"])
+            }
+    
+            if (Frameworks[i] === "JavaScript") {
+                DetailFramework.push(["JavaScript", "bg-yellow-500/20 text-yellow-300 px-3 py-1 rounded-full text-sm w-fit"])
+            }
+    
+            if (Frameworks[i] === "Electron.js") {
+                DetailFramework.push(["Electron.js", "bg-blue-500/20 text-blue-300 px-3 py-1 rounded-full text-sm w-fit"])
+            }
         }
     }
 
@@ -49,6 +51,10 @@ export default function Project({Name, Description, Frameworks, Icon, code, down
 
     if (Icon === "Folder-Git") {
         IconContent = <FolderGit className="text-white w-9 h-9 p-1 mb-2 stroke-1.25 bg-gradient-to-br from-violet-500 to-green-500 rounded-lg"/>
+    }
+
+    if (Icon === "Book") {
+        IconContent = <BookA className="text-white w-9 h-9 p-1 mb-2 stroke-1.25 bg-gradient-to-br from-blue-500 to-slate-500 rounded-lg" />
     }
 
     const isExternalUrl = (url: string) => {
@@ -99,7 +105,7 @@ export default function Project({Name, Description, Frameworks, Icon, code, down
                     )}
                 </span>
             </h1>
-            <p className="text-white text-left mb-5">
+            <p className="text-white text-left mb-5 whitespace-pre-wrap">
                 {Description}
             </p>
             <div className="flex flex-row gap-2">
