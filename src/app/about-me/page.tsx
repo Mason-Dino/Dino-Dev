d2c76ecda743fe8e0e646a2323aa84f94af12ccb
Mason-Dino <mason.n.dinosaur@gmail.com>
2025-09-04 22:40:47 +0000

    add an about me page

---
+++ src/app/about-me/page.tsx
@@ -0,0 +1,13 @@
+import Image from 'next/image';
+import Link from 'next/link'
+
+import { ChevronRight } from "lucide-react";
+import { Star, Bug, BookLock } from 'lucide-react';
+
+export default function PlateScout() {
+	return (
+		<div>
+			
+		</div>
+	);
+}
