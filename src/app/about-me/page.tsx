a35df7cfa941546de4b31822b2d66b6407afb157
Mason-Dino <mason.n.dinosaur@gmail.com>
2025-09-04 23:50:39 +0000

    added linkin and then also added 2 more jobs

--- src/app/about-me/page.tsx
+++ src/app/about-me/page.tsx
@@ -30,13 +30,16 @@ export default function PlateScout() {
                     <br />
                     <div id="icons" className='flex flex-row justify-center gap-2 text-white'>
                         <Link href="https://www.instagram.com/dinosaur__dev/" target='_blank'>
-                            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><rect width="20" height="20" x="2" y="2" rx="5" ry="5"/><path d="M16 11.37A4 4 0 1 1 12.63 8 4 4 0 0 1 16 11.37z"/><line x1="17.5" x2="17.51" y1="6.5" y2="6.5"/></svg>
+                            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"><rect width="20" height="20" x="2" y="2" rx="5" ry="5"/><path d="M16 11.37A4 4 0 1 1 12.63 8 4 4 0 0 1 16 11.37z"/><line x1="17.5" x2="17.51" y1="6.5" y2="6.5"/></svg>
                         </Link>
                         <Link href="mailto:mason.n.dinosaur@gmail.com" target='_blank'>
                             <Mail />
                         </Link>
                         <Link href="https://github.com/Mason-Dino" target='_blank'>
-                            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><path d="M15 22v-4a4.8 4.8 0 0 0-1-3.5c3 0 6-2 6-5.5.08-1.25-.27-2.48-1-3.5.28-1.15.28-2.35 0-3.5 0 0-1 0-3 1.5-2.64-.5-5.36-.5-8 0C6 2 5 2 5 2c-.3 1.15-.3 2.35 0 3.5A5.403 5.403 0 0 0 4 9c0 3.5 3 5.5 6 5.5-.39.49-.68 1.05-.85 1.65-.17.6-.22 1.23-.15 1.85v4"/><path d="M9 18c-4.51 2-5-2-7-2"/></svg>
+                            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"><path d="M15 22v-4a4.8 4.8 0 0 0-1-3.5c3 0 6-2 6-5.5.08-1.25-.27-2.48-1-3.5.28-1.15.28-2.35 0-3.5 0 0-1 0-3 1.5-2.64-.5-5.36-.5-8 0C6 2 5 2 5 2c-.3 1.15-.3 2.35 0 3.5A5.403 5.403 0 0 0 4 9c0 3.5 3 5.5 6 5.5-.39.49-.68 1.05-.85 1.65-.17.6-.22 1.23-.15 1.85v4"/><path d="M9 18c-4.51 2-5-2-7-2"/></svg>
+                        </Link>
+                        <Link href="https://www.linkedin.com/in/mason-norberg-920bab241/" target='_blank'>
+                            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"><path d="M16 8a6 6 0 0 1 6 6v7h-4v-7a2 2 0 0 0-2-2 2 2 0 0 0-2 2v7h-4v-7a6 6 0 0 1 6-6z"/><rect width="4" height="12" x="2" y="9"/><circle cx="4" cy="4" r="2"/></svg>
                         </Link>
                     </div>
                 </div>
@@ -46,9 +49,27 @@ export default function PlateScout() {
 					Experience
 				</h1>
                 <Project 
-                    Name="Governors Cyber Academy - Cyber Academy Enrichment Leader"
+                    Name="Cyber Academy Enrichment Leader"
                     Description={"Sep 2024 - Present\nWorked as the student leader for the Governors Cyber Academy which means I was the main student, I helped the Director of the Governors Cyber Academy with duties when they were unable to complete them.\n\nI help provide lessons to High Schoolers, Middle Schoolers, Elementary Schoolers, and Home School students to provide Cyber related activities for all from programming to soldering!"}
                     Icon="Book"
+                    code={false}
+                    download={false}
+                    website={false}
+                />
+                <br />
+                <Project 
+                    Name="Resident Assistant"
+                    Description={"Jan 2025 - Present\nServed as a Resident Assistant, fostering a safe, inclusive, and supportive living environment for over 38 residents. Planned and executed community-building programs, resolved conflicts, and enforced university policies to promote personal and academic success."}
+                    Icon="User"
+                    code={false}
+                    download={false}
+                    website={false}
+                />
+                <br />
+                <Project 
+                    Name="Camp Cyber Student Leader Team Coordinator"
+                    Description={"Nov 2024 - June 2025\nAided the development of the first-ever Camp Cyber from initial concept to full execution as a member of the core planning committee. Coordinate a team of 12 camp counselors, ensuring effective communication, task delegation, and overall efficiency throughout the camp. Act as the central point of contact between leadership and counselor teams, problem-solving in real time to support a smooth camper experience."}
+                    Icon="Caravan-DSU"
                     code={false}
                     download={false}
                     website={false}
