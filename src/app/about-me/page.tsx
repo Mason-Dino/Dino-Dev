eb79439a5d9688e7f7570b868caca3a6c685a339
Mason-Dino <mason.n.dinosaur@gmail.com>
2025-09-04 23:28:34 +0000

    added GCA

--- src/app/about-me/page.tsx
+++ src/app/about-me/page.tsx
@@ -1,6 +1,8 @@
 import Image from 'next/image';
 import Link from 'next/link'
 
+import Project from '../components/project';
+
 import { ChevronRight } from "lucide-react";
 import { Star, Bug, BookLock } from 'lucide-react';
 
@@ -23,6 +25,19 @@ export default function PlateScout() {
                         as of now I'm in my 2nd year or sophomore year at DSU!
                     </p>
                 </div>
+            </section>
+            <section className="flex flex-col text-center justify-content items-center">
+                <h1 id="projects" className="text-2xl md:text-2xl font-bold text-white my-6">
+					Experience
+				</h1>
+                <Project 
+                    Name="Governors Cyber Academy - Cyber Academy Enrichment Leader"
+                    Description={"Sep 2024 - Present\nWorked as the student leader for the Governors Cyber Academy which means I was the main student, I helped the Director of the Governors Cyber Academy with duties when they were unable to complete them.\n\nI help provide lessons to High Schoolers, Middle Schoolers, Elementary Schoolers, and Home School students to provide Cyber related activities for all from programming to soldering!"}
+                    Icon="Book"
+                    code={false}
+                    download={false}
+                    website={false}
+                />
             </section>
 		</div>
 	);
